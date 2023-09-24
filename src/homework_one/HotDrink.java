0759a3d62fd0b3ffb21a2c53f327695bba92bc2d
Denis <denis13071981@gmail.com>
2023-09-24 19:36:34 +0300

    Initial commit

---
+++ src/homework_one/HotDrink.java
@@ -0,0 +1,70 @@
+package homework_one;
+
+public class HotDrink extends Product{
+    private int volume;
+    private int temperature;
+
+    public HotDrink(String name, double cost) {
+        super(name, cost);
+    }
+
+    public HotDrink(String name, int volume, int temperature) {
+        super(name);
+        this.volume = volume;
+        this.temperature = temperature;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "BottleOfWater{" +
+                "name = " + super.getName() +
+                " \\ volume = " + this.volume +
+                " \\ temperature = " + this.temperature + "°C}\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof HotDrink that)) {
+            return false;
+        }
+
+        return super.getName().equalsIgnoreCase(that.getName())
+                && super.getCost() == that.getCost()
+                && getVolume() == that.getVolume();
+    }
+
+    public enum Drinks{
+        Coffee("Coffee"),
+        Tea("Tea"),
+        Cacao("Cacao");
+
+        private final String drinkName;
+        private Drinks(String drink){
+            this.drinkName = drink;
+        }
+        @Override
+        public String toString(){
+            return drinkName;
+        }
+
+    }
+}
---
+++ src/homework_one/Main.java
@@ -0,0 +1,10 @@
+package homework_one;
+public class Main {
+    public static void main(String[] args) {
+        HotDrink hotDrink = new HotDrink(
+                "Coffee",
+                100,
+                97);
+
+    }
+}
